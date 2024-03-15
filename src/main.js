dc3ab110e622d289362e84a7f502e44038e6b6d3
chenzhouheng <chenzh@nengfeng.com.cn>
2024-03-15 16:28:53 +0800

    表格提交

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
+/*
+ * @FileDescription: 
+ * @Author: czh
+ * @Date: 2024-03-13 15:46:30
+ * @LastEditors: czh
+ * @LastEditTime: 2024-03-13 16:11:56
+ */
 import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
 
+import './components/component'
+
 Vue.config.productionTip = false
+Vue.use(ElementUI);
 
 new Vue({
   render: h => h(App),
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
+/*
+ * @FileDescription: 
+ * @Author: czh
+ * @Date: 2024-03-13 15:46:30
+ * @LastEditors: czh
+ * @LastEditTime: 2024-03-13 16:44:10
+ */
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  lintOnSave:false,
   transpileDependencies: true
 })
