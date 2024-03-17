c6889c9eca11d5f5b7166a78e858b3f21a1b6eef
chenzhouheng <chenzh@nengfeng.com.cn>
2024-03-17 17:32:12 +0800

    组件使用优化

--- examples/main.js
+++ examples/main.js
@@ -3,14 +3,16 @@
  * @Author: czh
  * @Date: 2024-03-13 15:46:30
  * @LastEditors: czh
- * @LastEditTime: 2024-03-13 16:11:56
+ * @LastEditTime: 2024-03-17 17:30:25
  */
 import Vue from 'vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
 
-import './components/component'
+import BaseTable from '../packages/BaseTable/index'
+
+Vue.component('BaseTable', BaseTable)
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
